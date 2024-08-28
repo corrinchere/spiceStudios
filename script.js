fed5b686da93728b26186ed77ff4bf8e1be64e9c
corrinchere <96142555+corrinchere@users.noreply.github.com>
2024-08-27 17:22:46 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+document.getElementById('ham-menu').addEventListener('click', function(){
+    document.getElementById('ham-menu-open').classList.add('toggle-open');
+} )
+
+document.getElementById('ham-menu-close').addEventListener('click', function(){
+    document.getElementById('ham-menu-open').classList.remove('toggle-open');
+} )
+
